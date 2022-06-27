51be7dd2638fabe037a082b03610b7f7af56a4ae
Shu <shu@Shus-MacBook-Pro.local>
2022-06-27 18:01:23 +0630

    project setup and title and task view

---
+++ todolist/App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.taskWrapper}>
+      <Text style={styles.sectionTitle}>Today's Tasks</Text>
+      
+      <View style={styles.items}>
+      <Task text={'Task 1'}/>
+      <Task text={'Task 2'}/>
+      </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
+  },
+});
